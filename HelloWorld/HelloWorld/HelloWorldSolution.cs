3dd3a78eaf47cfdbcd7ff131c026e8f9f21f91c2
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-18 18:42:54 +0900

    221018 1~3장 환경 구축 및 첫 작성
      컴퓨터에 VS가 여러개 깔려있는 바람에 VS19 버전에서 .net 이용해서 시작하는데 시간이 조금 들었지만 첫 출력 성공.
    
      구조, 원리, 명칭을 정확히 기억하고 넘어가야 하겠다는 생각이 듬.

---
+++ HelloWorld/HelloWorld/HelloWorldSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+namespace HelloWorld
+{
+    class HelloWorldSolution
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("First Running~^^");
+        }
+    }
+}*/
+
+class CaseSensitive
+{
+    static void Main()
+    {
+        WriteLine("C#은 대소문자 구분 언어.");
+    }
+}
