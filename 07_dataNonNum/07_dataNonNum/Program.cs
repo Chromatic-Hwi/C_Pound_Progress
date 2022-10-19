328fccd6b7ee4ebb6ec8f77b59dc186f88e7ec64
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-19 22:10:58 +0900

    221019 9장까지 학습
      var 형식을 배우기 전에 입력과 출력을 하는 중에 데이터 형식 변환과 참조에서 잠시 시간이 걸렸는데 금방 해결했고, 문자열 정리나 출력 등에서도 응용이 금방 되는 편.
    
      아직 기초인지라 개념은 확실히 하고 넘어가되 속도를 좀 더 낼 필요가 있음.

---
+++ 07_dataNonNum/07_dataNonNum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_dataNonNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            char grade = 'A';
+            char kor = '가';
+            string name = "김성모";
+            Console.WriteLine(grade);
+            Console.WriteLine(kor);
+            Console.WriteLine(name);
+            */
+
+            int phoneNum = 222_3456;
+            string name = "Hwi";
+            string msg = @"Welcome!
+C# class!";
+            bool pass = true;
+            Console.WriteLine($"{DateTime.Now}\n\nGreetings! {name}\n\nWe check your P.H as {phoneNum}\n{msg}\nYour test condition is {pass}");
+        }
+    }
+}
---
+++ 08_Read/08_Read/Read.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Read
+{
+    /*
+    class Read
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type message>> ");
+            string msg = Console.ReadLine();
+            Console.WriteLine($"Your message is \"{msg}\"");
+        }
+    }*/
+
+    /*
+    class IntToByte
+    {
+        static void Main()
+        {
+            int x = 255;
+            byte y = (byte)x;
+
+            Console.WriteLine($"{x} => {y}");
+        }
+    }*/
+
+    class BinaryString
+    {
+        static void Main()
+        {
+            Console.Write("Type origin number>> ");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.WriteLine($"You typed {y}\n");
+
+            string yB = Convert.ToString(y, 2).PadLeft(8, '0');
+            Console.Write("Your number\'s type is converted to ");
+            Console.WriteLine("\'"+yB.GetType()+"\'\n");
+            Console.Write("Converted binary number => ");
+            Console.WriteLine(yB+"\n");
+
+            byte b1 = 0b0010;
+            Console.WriteLine(b1);
+        }
+    
+    }
+}
