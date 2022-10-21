04b0f1ee2d6a7c700c3909db6fa3952612371955
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-21 22:53:17 +0900

    221021 15강 for문 학습
      아직은 간단한 for문 연습. 3D 프린트 설계와 만남 약속으로 인해 진도 부진.

---
+++ 15_for/15_for/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_for
+{
+    class Triangle
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+                {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
