e371607109aaf189ac359cf308d6af87e282bf56
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-22 23:48:03 +0900

    221022 가변배열까지 학습
      새로운 언어라고 얼타는지 찬찬히 보면 원리도 똑같고, 오류가 보이는데 오타를 내거나 변수 참조를  잘못 해서 오류가 나는 경우가 종종 있음. 침착하게 공부하자.

---
+++ 18_ZigZag/18_ZigZag/ZigZag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_ZigZag
+{
+    class ZigZag
+    {
+        static void Main()
+        {
+            int[][] zagArray = new int[2][];
+
+            zagArray[0] = new int[] { 1, 2 };
+            zagArray[1] = new int[] { 3, 4, 5 };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < zagArray[i].Length; j++)
+                {
+                    Console.Write($"{zagArray[i][j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
