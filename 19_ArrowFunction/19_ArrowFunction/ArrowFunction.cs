176e56ff4726e93684dc2473c3a342597345f49f
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-23 11:07:25 +0900

    19강 함수
      명령줄 인수는 일단 공부만 하고 넘어감.

---
+++ 19_ArrowFunction/19_ArrowFunction/ArrowFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_ArrowFunction
+{
+    class ArrowFunction
+    {
+        static void Hi() => Console.WriteLine("Hello!");
+        static void Main()
+        {
+            Hi();
+        }
+    }
+}
---
+++ 19_FunctionCall/19_FunctionCall/FucntionCall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_FunctionCall
+{
+    class FucntionCall
+    {
+        static void Msg(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static double CircleArea(double radius)
+        {
+            ///<summary>
+            /// 반지름을 입력 받아 원넓이를 구하는 함수. 데이터 형식 오류 때문에 double 로 만들어봤음.
+            ///</summary>
+            
+            Console.Write("Type the radius >> ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            double circle = radius * radius * 3.14f;
+            return circle;
+        }
+
+        static void Main()
+        {
+            //for (int i = 0; i <= 2; i++) { Msg("parameter"); }
+            Console.WriteLine(CircleArea(5));
+        }
+    }
+}
