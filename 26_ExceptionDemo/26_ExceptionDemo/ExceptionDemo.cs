38a74e5d1a5ddc3306bc83e77eb2b2e239659aa1
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-27 22:15:20 +0900

    221027 26장 예외처리까지 공부
      클래스 파트부터 속도가 많이 느려졌음. 좀 더 분발하면서 C# 코테를 통한 기반 다지기도 꾸준히 해야겠음.

---
+++ 26_ExceptionDemo/26_ExceptionDemo/ExceptionDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_ExceptionDemo
+{
+    class ExceptionDemo
+    {
+        static void Main()
+        {
+            try
+            {
+                int[] arr = new int[2];
+                arr[100] = 1234;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
