f137c586144695be4cbe3b59af327ee8ff0eed1d
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-28 13:36:29 +0900

    221028 컬렉션 학습

---
+++ 27_ArrayClass/27_ArrayClass/ArrayClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_ArrayClass
+{
+    class ArrayClass
+    {
+        static void Main()
+        {
+            int[] arr = { 3, 2, 1, 4, 5 };
+            Array.Sort(arr);
+            foreach (var item in arr) { Console.WriteLine(item); }
+            Console.WriteLine();
+
+            int[] arr2 = { 5, 4, 3, 2, 1 };
+            Array.Reverse(arr2);
+            foreach (var item in arr2) { Console.WriteLine(item); }
+        }
+    }
+}
