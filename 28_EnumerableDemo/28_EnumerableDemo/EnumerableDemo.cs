945ae01a564aebb7b6cf1e40304ced7acb5baea7
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-29 23:34:20 +0900

    221029 제네릭과 널 제네릭, 연산자 학습
      제네릭 부분은 실습 예제도 부족하고 이해가 잘 안되서 다른 인터넷 자료나 영상으로 반드시 추가 공부가 필요하고, null의 경우는 파이썬과 다르게 데이터 형식과, 사용을 위한 조건, 연산자 등이 다르기 때문에 새롭게 기능을 공부했음. ? 연산자 실습도 좀 더 연습해볼 필요가 있음.

---
+++ 28_EnumerableDemo/28_EnumerableDemo/EnumerableDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_EnumerableDemo
+{
+    class EnumerableDemo
+    {
+        static void Main()
+        {
+            var numbers = Enumerable.Range(1, 5); // 1 ~ 5
+            foreach (var n in numbers)
+                Console.Write("{0}\t", n);
+            Console.WriteLine();
+
+            var sameNumbers = Enumerable.Repeat(-1, 5); // -1 5ea
+            foreach (var n in sameNumbers)
+                Console.Write("{0}\t", n);
+            Console.WriteLine();
+        }
+    }
+}
