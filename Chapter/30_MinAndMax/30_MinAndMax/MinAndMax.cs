d61230e9978a5fb3fa2695f6119f5b5230434ad6
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-30 23:54:53 +0900

    221030 파일 정리 및 LINQ 실습
      시간 부족으로 인해 30강 LINQ 에서도 기본 메서드 실습만 했음.

---
+++ Chapter/30_MinAndMax/30_MinAndMax/MinAndMax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_MinAndMax
+{
+    class MinAndMax
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3 };
+            int min = arr.Min();
+            int max = arr.Max();
+
+            Console.WriteLine($"Min Value : {min}, Max Value : {max}");
+        }
+    }
+}
