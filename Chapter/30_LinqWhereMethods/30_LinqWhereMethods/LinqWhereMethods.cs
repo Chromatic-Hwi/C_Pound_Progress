72bca5ec548bc536624287c6a11d10b3a57e337a
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-31 23:56:39 +0900

    221031 LINQ 람다식과 where 메서드 실습
      메서드를 활용해서 조건 처리를 해보았는데,, 아직은 여러 코드를 다뤄보지 않아서 일반 조건문과 큰 차이를 못 느끼겠음. 개념적인 부분은 진도를 빨리 빼고 코테 문제를 풀어보면서 감을 잡아야 할 필요가 있음.

---
+++ Chapter/30_LinqWhereMethods/30_LinqWhereMethods/LinqWhereMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_LinqWhereMethods
+{
+    class LinqWhereMethods
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            var q = arr.Where(num => num % 2 == 1);
+
+            foreach (var n in q)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ Chapter/30_WhereFilter/30_WhereFilter/WhereFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_WhereFilter
+{
+    class WhereFilter
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            var nums = numbers.Where(it => it % 2 == 0 && it > 3);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
