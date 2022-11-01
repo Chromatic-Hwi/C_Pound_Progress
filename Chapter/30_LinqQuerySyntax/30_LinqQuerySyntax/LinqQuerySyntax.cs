912eda28dd76235f7f0fbe262f6ec01898c8fad3
Seo Hwi <chromatic_365@naver.com>
2022-11-01 23:35:52 +0900

    221101 LINQ 실습
      메서드 기법과 쿼리 기법 실습,
    
    Select() 확장 메서드로 실습 해봤는데 솔직히 아직까진 Select 확장 메서드가 어떤 장점이나 필요성이 있는지 체감은 잘 안되고 여러 코드들을 만지면서 C# 언어에 익숙해져 간다~ 느낌은 있음.

---
+++ Chapter/30_LinqQuerySyntax/30_LinqQuerySyntax/LinqQuerySyntax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_LinqQuerySyntax
+{
+    class LinqQuerySyntax
+    {
+        static void Main()
+        {
+            int[] numbers = { 3, 2, 1, 4, 5 };
+
+            // 메서드 구문
+            IEnumerable<int> methodSyntax =
+                numbers.Where(n => n % 2 == 1).OrderBy(n => n);
+
+            foreach (var n in methodSyntax)
+            {
+                Console.WriteLine(n);
+            }
+
+            // 쿼리 구문
+            IEnumerable<int> querySyntax =
+                from num in numbers
+                where num % 2 == 1
+                orderby num
+                select num;
+
+            foreach (var n in querySyntax)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ Chapter/30_Map/30_Map/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Map
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            var nums = numbers.Select(it => it * it);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
