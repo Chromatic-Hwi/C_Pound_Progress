7f3006a8dacd76311e77101e5556f0950c2d97f9
Seo Hwi <chromatic_365@naver.com>
2022-11-02 10:40:47 +0900

    221102

---
+++ Chapter/31_AverageAlgorithm/31_AverageAlgorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_AverageAlgorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] data = { 90, 65, 78, 50, 95 };
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] >= 90 && data[i] <= 95)
+                {
+                    sum += data[i];
+                    count++;
+                }
+            }
+
+            double avg = 0.0f;
+            if (sum != 0 && count != 0)
+            {
+                avg = sum / (double)count;
+            }
+
+            Console.WriteLine(avg);
+        }
+    }
+}
---
+++ Chapter/31_SumAlgorithm/31_SumAlgorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_SumAlgorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] scores = { 100, 75, 50, 37, 90, 95 };
+            int sum = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] >= 80)
+                {
+                    sum += scores[i];
+                }
+            }
+
+            Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상 총점 : {sum}");
+        }
+    }
+}
